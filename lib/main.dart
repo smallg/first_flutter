3187c5034c07bf96a5ebe61693830085476ff77e
smallg <gwpxjb@163.com>
2019-06-16 22:18:15 +0800

    add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter/demo/bottom_navigator.dart';
 import 'package:first_flutter/demo/drawer.dart';
 import 'package:flutter/material.dart';
 
@@ -10,7 +11,7 @@ class App extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: Home(),
         theme: ThemeData(
-            primarySwatch: Colors.yellow,
+            primarySwatch: Colors.deepPurple,
             highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
             splashColor: Colors.white70));
   }
@@ -22,35 +23,36 @@ class Home extends StatelessWidget {
     return DefaultTabController(
         length: 3,
         child: Scaffold(
-            backgroundColor: Colors.grey[100],
-            appBar: AppBar(
-                title: Text('Home X'),
-                actions: <Widget>[
-                  IconButton(
-                      icon: Icon(Icons.search),
-                      tooltip: 'Search',
-                      onPressed: () =>
-                          {debugPrint('search button is pressed!')})
-                ],
-                elevation: 0.0,
-                bottom: TabBar(
-                  unselectedLabelColor: Colors.black38,
-                  indicatorColor: Colors.black54,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  indicatorWeight: 1.0,
-                  tabs: <Widget>[
-                    Tab(icon: Icon(Icons.local_florist)),
-                    Tab(icon: Icon(Icons.change_history)),
-                    Tab(icon: Icon(Icons.directions_bike)),
-                  ],
-                )),
-            body: TabBarView(
-              children: <Widget>[
-                Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
-                Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-                Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+              title: Text('Home X'),
+              actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.search),
+                    tooltip: 'Search',
+                    onPressed: () => {debugPrint('search button is pressed!')})
               ],
-            ),
-            drawer: DrawerDemo()));
+              elevation: 0.0,
+              bottom: TabBar(
+                unselectedLabelColor: Colors.black38,
+                indicatorColor: Colors.black54,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorWeight: 1.0,
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.local_florist)),
+                  Tab(icon: Icon(Icons.change_history)),
+                  Tab(icon: Icon(Icons.directions_bike)),
+                ],
+              )),
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            ],
+          ),
+          drawer: DrawerDemo(),
+          bottomNavigationBar: BottomNavigationBarDemo(),
+        ));
   }
 }
