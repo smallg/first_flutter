c8dc5e19611f251d5701ddd2f96fc8ce427268a7
smallg <gwpxjb@163.com>
2019-06-16 21:26:20 +0800

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,10 @@ class App extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Home(),
-        theme: ThemeData(primarySwatch: Colors.yellow));
+        theme: ThemeData(
+            primarySwatch: Colors.yellow,
+            highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+            splashColor: Colors.white70));
   }
 }
 
@@ -19,39 +22,46 @@ class Home extends StatelessWidget {
     return DefaultTabController(
         length: 3,
         child: Scaffold(
-            backgroundColor: Colors.grey[100],
-            appBar: AppBar(
-                leading: IconButton(
-                    icon: Icon(Icons.menu),
-                    tooltip: 'Navigration',
-                    onPressed: () =>
-                        {debugPrint('Navigration button is pressed!')}),
-                title: Text('Home X'),
-                actions: <Widget>[
-                  IconButton(
-                      icon: Icon(Icons.search),
-                      tooltip: 'Search',
-                      onPressed: () =>
-                          {debugPrint('search button is pressed!')})
-                ],
-                elevation: 0.0,
-                bottom: TabBar(
-                  unselectedLabelColor: Colors.black38,
-                  indicatorColor: Colors.black54,
-                  indicatorSize: TabBarIndicatorSize.label,
-                  indicatorWeight: 1.0,
-                  tabs: <Widget>[
-                    Tab(icon: Icon(Icons.local_florist)),
-                    Tab(icon: Icon(Icons.change_history)),
-                    Tab(icon: Icon(Icons.directions_bike)),
-                  ],
-                )),
-            body: TabBarView(
-              children: <Widget>[
-                Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
-                Icon(Icons.change_history, size: 128.0, color: Colors.black12),
-                Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+              leading: IconButton(
+                  icon: Icon(Icons.menu),
+                  tooltip: 'Navigration',
+                  onPressed: () =>
+                      {debugPrint('Navigration button is pressed!')}),
+              title: Text('Home X'),
+              actions: <Widget>[
+                IconButton(
+                    icon: Icon(Icons.search),
+                    tooltip: 'Search',
+                    onPressed: () => {debugPrint('search button is pressed!')})
               ],
-            )));
+              elevation: 0.0,
+              bottom: TabBar(
+                unselectedLabelColor: Colors.black38,
+                indicatorColor: Colors.black54,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorWeight: 1.0,
+                tabs: <Widget>[
+                  Tab(icon: Icon(Icons.local_florist)),
+                  Tab(icon: Icon(Icons.change_history)),
+                  Tab(icon: Icon(Icons.directions_bike)),
+                ],
+              )),
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            ],
+          ),
+          drawer: Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[Text('This is drawer')],
+              )),
+        ));
   }
 }
