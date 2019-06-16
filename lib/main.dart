3c34ce5e5d47d830e2e7e203e1e6f71150817955
smallg <gwpxjb@163.com>
2019-06-16 20:18:41 +0800

    section2: list view

--- lib/main.dart
+++ lib/main.dart
@@ -3,53 +3,38 @@ import 'model/post.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
-      theme: ThemeData(
-        primarySwatch: Colors.yellow
-      )
-    );
+        home: Home(), theme: ThemeData(primarySwatch: Colors.yellow));
   }
 }
 
 class Home extends StatelessWidget {
   Widget _listItemBuilder(BuildContext context, int index) {
     return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child:Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-            posts[index].title,
-            style:Theme.of(context).textTheme.title
-          ),
-          Text(
-            posts[index].title,
-            style:Theme.of(context).textTheme.subhead
-          )
-        ],
-      )
-    );
+        color: Colors.white,
+        margin: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Image.network(posts[index].imageUrl),
+            SizedBox(height: 16.0),
+            Text(posts[index].title, style: Theme.of(context).textTheme.title),
+            Text(posts[index].author, style: Theme.of(context).textTheme.subhead)
+          ],
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        title: Text('Home'),
-        elevation: 0.0,
-      ),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: _listItemBuilder
-
-      )
-    );
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          title: Text('Home'),
+          elevation: 0.0,
+        ),
+        body: ListView.builder(
+            itemCount: posts.length, itemBuilder: _listItemBuilder));
   }
 }
