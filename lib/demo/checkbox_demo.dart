aaa82ad6d26e1a62a44d09100bd0c04f6ce2f5f5
smallg <gwpxjb@163.com>
2019-06-24 20:40:29 +0800

    Section11: Checkbox/Radio/Switch

--- lib/demo/checkbox_demo.dart
+++ lib/demo/checkbox_demo.dart
@@ -7,6 +7,14 @@ class CheckboxDemo extends StatefulWidget {
 
 class _CheckboxDemoState extends State<CheckboxDemo> {
   bool _checkboxItemA = true;
+  int _radioGroupA = 0;
+  bool _switchItemA = false;
+  double _sliderItemA = 0.0;
+  void _handleRadioValueChanged(int value) {
+    setState(() {
+      _radioGroupA = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,9 +40,81 @@ class _CheckboxDemoState extends State<CheckboxDemo> {
               secondary: Icon(Icons.bookmark),
               selected: _checkboxItemA,
             ),
+            Text('RadioGroupValue: $_radioGroupA'),
+            RadioListTile(
+              value: 0,
+              groupValue: _radioGroupA,
+              onChanged: _handleRadioValueChanged,
+              title: Text('Options A'),
+              subtitle: Text('Description'),
+              secondary: Icon(Icons.filter_1),
+              selected: _radioGroupA == 0,
+            ),
+            RadioListTile(
+              value: 1,
+              groupValue: _radioGroupA,
+              onChanged: _handleRadioValueChanged,
+              title: Text('Options A'),
+              subtitle: Text('Description'),
+              secondary: Icon(Icons.filter_2),
+              selected: _radioGroupA == 1,
+            ),
+            SwitchListTile(
+              value: _switchItemA,
+              onChanged: (value) {
+                setState(() {
+                  _switchItemA = value;
+                });
+              },
+              title: Text('Switch Item A'),
+              subtitle: Text('Description'),
+              secondary: Icon(
+                _switchItemA ? Icons.visibility : Icons.visibility_off,
+              ),
+              selected: _switchItemA,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                Slider(
+                  value: _sliderItemA,
+                  onChanged: (value) {
+                    setState(() {
+                      _sliderItemA = value;
+                    });
+                  },
+                  activeColor: Theme.of(context).accentColor,
+                  inactiveColor: Theme.of(context).accentColor.withOpacity(0.3),
+                  min: 0.0,
+                  max: 10.0,
+                  divisions: 10,
+                  label: '${_sliderItemA.toInt()}',
+                ),
+                Text('SliderValue: $_sliderItemA'),
+                // Text(
+                //   _switchItemA ? '😊' : '😢',
+                //   style: TextStyle(fontSize: 32.0),
+                // ),
+                // Switch(
+                //   value: _switchItemA,
+                //   onChanged: (value) {
+                //     setState(() {
+                //       _switchItemA = value;
+                //     });
+                //   },
+                // )
+                // Radio(
+                //   value: 0,
+                //   groupValue: _radioGroupA,
+                //   onChanged: _handleRadioValueChanged,
+                //   activeColor: Colors.black,
+                // ),
+                // Radio(
+                //   value: 1,
+                //   groupValue: _radioGroupA,
+                //   onChanged: _handleRadioValueChanged,
+                //   activeColor: Colors.black,
+                // ),
                 // Checkbox(
                 //   value: _checkboxItemA,
                 //   activeColor: Colors.black,
