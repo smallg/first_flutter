32eb2a72a1bf6737d1248eef325c2f4361df6f81
smallg <gwpxjb@163.com>
2019-06-28 20:13:56 +0800

    Section 16: Subject

--- lib/demo/rxdart/rxdart_demo.dart
+++ lib/demo/rxdart/rxdart_demo.dart
@@ -21,20 +21,62 @@ class RxDartDemoHome extends StatefulWidget {
 }
 
 class _RxDartDemoHomeState extends State<RxDartDemoHome> {
+  PublishSubject<String> _textFieldSubject;
+
   @override
   void initState() {
     super.initState();
-    Observable<String> _observable =
-        // Observable(Stream.fromIterable(['hello', '您好']));
-        // Observable.fromFuture(Future.value(('hello~')));
-        // Observable.fromIterable(['hello', '您好']);
-        // Observable.just('Hello!');
-        Observable.periodic(Duration(seconds: 3), (x) => x.toString());
-    _observable.listen(print);
+    _textFieldSubject = PublishSubject<String>();
+    _textFieldSubject
+        // .map((item) => 'item: $item')
+        // .where((item) => item.length > 9)
+        .debounceTime(Duration(milliseconds: 500))
+        .listen((data) => print(data));
+
+    // Observable<String> _observable =
+    // Observable(Stream.fromIterable(['hello', '您好']));
+    // Observable.fromFuture(Future.value(('hello~')));
+    // Observable.fromIterable(['hello', '您好']);
+    // Observable.just('Hello!');
+    // Observable.periodic(Duration(seconds: 3), (x) => x.toString());
+    // _observable.listen(print);
+
+    // PublishSubject<String> _subject = PublishSubject<String>();
+    // BehaviorSubject<String> _subject = BehaviorSubject<String>();
+    // ReplaySubject<String> _subject = ReplaySubject<String>(maxSize: 2);
+
+    // _subject.add('hello');
+    // _subject.add('hi');
+    // _subject.add('hey');
+    // _subject.listen((data) => print('listen 1: $data'));
+    // _subject.listen((data) => print('listen 2: ${data.toUpperCase()}'));
+    // _subject.close();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _textFieldSubject.close();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Theme(
+      data: Theme.of(context).copyWith(
+        primaryColor: Colors.black,
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'Title',
+          filled: true,
+        ),
+        onChanged: (value) {
+          _textFieldSubject.add('input: $value');
+        },
+        onSubmitted: (value) {
+          _textFieldSubject.add('submit: $value');
+        },
+      ),
+    );
   }
 }
